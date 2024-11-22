015d2db98431ce013f65e86c81e52af827076858
Will Blanchard <Blanchardpwilliam3@gmail.com>
2024-11-22 14:29:02 +0100

    Did some class reformatting to better suit my coding conventions. Not much in this commit
    Additions |
    
    NONE
    
    Major Updates |
    
    NONE
    
    Menial Updates |
    
    - Formatted all classes to be in line with my conventions.
    
    - Changed Mod Version
    
    - Added some headers while formatting build.gradle but then realized imma switch to kotlin build script. So I just left it half finished.
    
    - Updated the link to the GitHub repository in fabric.mod.json.
    
    - Updated both mixins.json files. Removing the example mixins.
    
    Removals |
    
    - Removed the example mixins
    
    Patches |
    
    NONE

--- src/client/java/lu/embellishedduck/client/TandemCavernsClient.java
+++ src/client/java/lu/embellishedduck/client/TandemCavernsClient.java
@@ -1,10 +1,15 @@
 package lu.embellishedduck.client;
 
+import lu.embellishedduck.core.TandemCaverns;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TandemCavernsClient implements ClientModInitializer {
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
+
+		TandemCaverns.LOGGER.info("Client-Side is good to go! Awaiting instructions.");
+
+	}//End of Method
+
+}//End of Class
--- src/main/java/lu/embellishedduck/datagen/TandemCavernsDataGenerator.java
+++ src/main/java/lu/embellishedduck/datagen/TandemCavernsDataGenerator.java
@@ -4,8 +4,12 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class TandemCavernsDataGenerator implements DataGeneratorEntrypoint {
+
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
-	}
-}
+
+
+	}//End of Method
+
+}//End of Class
